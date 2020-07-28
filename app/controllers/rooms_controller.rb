774250c7cd148a40a7cdb02e681bac184331cd3e
muromoto-y <6610010@gmail.com>
2020-07-28 16:09:46 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,14 +3,17 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
 
-def create
-  @room = Room.new(room_params)
-  if @room.save
-    redirect_to root_path
-  else
-    render :new
+  def index
   end
-end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   private
 
